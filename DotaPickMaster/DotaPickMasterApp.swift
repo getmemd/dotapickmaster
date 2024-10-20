d3b748f1068d8024c4f5a1ef43ae7a26a5dd31f6
adil <amedeuyev@gmail.com>
2024-10-20 22:55:07 +0500

    Initial Commit

---
+++ DotaPickMaster/DotaPickMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaPickMasterApp.swift
+//  DotaPickMaster
+//
+//  Created by Adilkhan Medeuyev on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DotaPickMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DotaPickMasterTests/DotaPickMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  DotaPickMasterTests.swift
+//  DotaPickMasterTests
+//
+//  Created by Adilkhan Medeuyev on 20.10.2024.
+//
+
+import Testing
+@testable import DotaPickMaster
+
+struct DotaPickMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
